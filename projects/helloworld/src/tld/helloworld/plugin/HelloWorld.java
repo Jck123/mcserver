0242c5fd7df39c8a6effc46d1223fdd28beb888c
Jck123 <jck.1.2.3.a@gmail.com>
2021-02-24 22:53:53 -0500

    Now says hello to the user

--- projects/helloworld/src/tld/helloworld/plugin/HelloWorld.java
+++ projects/helloworld/src/tld/helloworld/plugin/HelloWorld.java
@@ -16,7 +16,7 @@ public class HelloWorld extends JavaPlugin {
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		if (command.getName().equalsIgnoreCase("hello")) {
-			sender.sendMessage(sender.getName() + "is a faggot!");
+			sender.sendMessage("Hello, " + sender.getName());
 			return true;
 		}
 		return false;
