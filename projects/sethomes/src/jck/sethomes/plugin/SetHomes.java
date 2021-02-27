d9c06f3b703972c0346261a270d54cfdcb013422
Jck123 <jck.1.2.3.a@gmail.com>
2021-02-27 01:09:28 -0500

    Added Jck Set Homes

---
+++ projects/sethomes/src/jck/sethomes/plugin/SetHomes.java
@@ -0,0 +1,26 @@
+package jck.sethomes.plugin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SetHomes extends JavaPlugin {
+	@Override
+	public void onEnable() {
+		
+	}
+	@Override
+	public void onDisable() {
+		
+	}
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if (command.getName().equalsIgnoreCase("GetPlLoc")) {
+			Player ply = Bukkit.getPlayer(args[0]);
+			sender.sendMessage(ply.getName() + " is at: " + ply.getLocation());
+			return true;
+		}
+		return false;
+	}
+}
